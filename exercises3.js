8147449a699dee7305002317645ea3484ba5049a
shantidyah <shanti.dyah@gmail.com>
2018-03-28 00:36:02 +0700

    if-else

---
+++ exercises3.js
@@ -0,0 +1,22 @@
+var nama='shanti';
+var peran='Ksatria';
+
+
+if(nama==''&&peran==''){
+  console.log("nama harus diisi");
+}
+else if (nama=='shanti'&&peran=='') {
+  console.log("Hallo shanti, Pilih peranmu untuk mulai game !");
+}
+else if (nama=='shanti'&&peran=='Ksatria') {
+  console.log("Selamat datang di Dunia Proxytia, Shanti");
+  console.log("Halo Ksatria Shanti, kamu dapat menyerang dengan senjatamu!");
+}
+else if (nama=='dyah'&&peran=='tabib') {
+  console.log("selamat datang di Dunia Proxytia, dyah");
+  console.log("hallo tabib dyah, kamu akan membantu temanmu yang terluka.");
+}
+else if (nama=='puspa'&&peran=='penyihir') {
+  console.log("selamat datang di dunia proxytia, puspa");
+  console.log("halo penyihir puspa, ciptakan kerajaan yang membantu kemenanganmu!");
+}
