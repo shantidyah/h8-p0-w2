0bca00b21104f412b526a09b79f6ab465c8e38bc
shantidyah <shanti.dyah@gmail.com>
2018-03-30 18:26:44 +0700

    tugas javascript

--- exercises4.js
+++ exercises4.js
@@ -1,57 +1,60 @@
 var hari='2'; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
 var bulan=3; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun='2001'; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var tampil='';
 
 switch (bulan) {
   case 1:{
-    console.log(hari,'Januari',tahun);
+    tampil=hari+' Januari '+tahun;
     break;
   }
   case 2:{
-    console.log(hari,'Februari',tahun);
+    tampil=hari+' Februari '+tahun;
     break;
   }
   case 3:{
-    console.log(hari,'Maret',tahun);
+    tampil=hari+' Maret '+tahun;
     break;
   }
   case 4:{
-    console.log(hari,'April',tahun);
+    tampil=hari+' April '+tahun;
     break;
   }
   case 5:{
-    console.log(hari,'Mei',tahun);
+    tampil=hari+' Mei '+tahun;
     break;
   }
   case 6:{
-    console.log(hari,'Juni',tahun);
+    tampil=hari+' Juni '+tahun;
     break;
   }
   case 7:{
-    console.log(hari,'Juli',tahun);
+    tampil=hari+' Juli '+tahun;
     break;
   }
   case 8:{
-    console.log(hari,'Agustus',tahun);
+    tampil=hari+' Agustus '+tahun;
     break;
   }
   case 9:{
-    console.log(hari,'September',tahun);
+    tampil=hari+' September '+tahun;
     break;
   }
   case 10:{
-    console.log(hari,'Oktober',tahun);
+    tampil=hari+' Oktober '+tahun;
     break;
   }
   case 11:{
-    console.log(hari,'November',tahun);
+    tampil=hari+' November '+tahun;
     break;
   }
   case 12:{
-    console.log(hari,'Desember',tahun);
+    tampil=hari+' Desember '+tahun;
     break;
   }
   default:{
-    console.log('bulan yang dimasukan salah');
+    tampil='bulan yang dimasukan salah';
   }
 }
+
+console.log(tampil);
