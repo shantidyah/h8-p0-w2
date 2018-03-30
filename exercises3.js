617acc7fcfa97a547bdf0ee21c3bfe0cd9a4fd3c
Shanti Dyah Pusparani <37474161+shantidyah@users.noreply.github.com>
2018-03-30 16:31:37 +0700

    Update exercises3.js

--- exercises3.js
+++ exercises3.js
@@ -2,21 +2,22 @@ var nama='shanti';
 var peran='Ksatria';
 
 
-if(nama==''&&peran==''){
+if(nama===''&&peran===''){
   console.log("nama harus diisi");
 }
-else if (nama=='shanti'&&peran=='') {
-  console.log("Hallo shanti, Pilih peranmu untuk mulai game !");
+else if (nama!==''&&peran==='') {
+  console.log("Hallo "+nama+", Pilih peranmu untuk mulai game !");
 }
-else if (nama=='shanti'&&peran=='Ksatria') {
-  console.log("Selamat datang di Dunia Proxytia, Shanti");
-  console.log("Halo Ksatria Shanti, kamu dapat menyerang dengan senjatamu!");
+else if (nama!==''&&peran==='Ksatria') {
+  console.log("Selamat datang di Dunia Proxytia, "+nama);
+  console.log("Halo "+peran+" "+nama+", kamu dapat menyerang dengan senjatamu!");
 }
-else if (nama=='dyah'&&peran=='tabib') {
-  console.log("selamat datang di Dunia Proxytia, dyah");
-  console.log("hallo tabib dyah, kamu akan membantu temanmu yang terluka.");
+else if (nama!==''&&peran==='tabib') {
+  console.log("selamat datang di Dunia Proxytia, "+nama);
+  console.log("hallo "+peran+" "+nama+", kamu akan membantu temanmu yang terluka.");
 }
-else if (nama=='puspa'&&peran=='penyihir') {
-  console.log("selamat datang di dunia proxytia, puspa");
-  console.log("halo penyihir puspa, ciptakan kerajaan yang membantu kemenanganmu!");
+else if (nama!==''&&peran==='penyihir') {
+  console.log("selamat datang di dunia proxytia, "+nama);
+  console.log("halo "+peran+" "+nama+", ciptakan kerajaan yang membantu kemenanganmu!");
 }
+
